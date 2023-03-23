9573117c6251584e0d69d407a21ffdc5d298daf1
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-23 13:05:02 -0300

    refactor: change the location of the files

---
+++ service3app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    code = models.CharField(max_length=20)
+    purchase_price = models.IntegerField()
+    sale_price = models.IntegerField()
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['code']
