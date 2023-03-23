1071c6ce92f1243625d0d326feecef91af151c7d
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-23 18:15:55 -0300

    refactor: update views.py and migrations

--- service3app/views.py
+++ service3app/views.py
@@ -1,6 +1,4 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Products
