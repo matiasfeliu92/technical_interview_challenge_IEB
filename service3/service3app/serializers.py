b602da3946919953dfa0712485d7fb2349d4885e
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-23 13:05:43 -0300

    refactor: delete old files

--- service3/service3app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Products
-
-class ProductsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Products
-        fields = ("code", "purchase_price", "sale_price", "description")
