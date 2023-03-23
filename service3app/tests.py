4f4b69329f17b5be669f1d578cba789f96570cc8
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-23 18:15:26 -0300

    refactor: add test file, for make api tests

--- service3app/tests.py
+++ service3app/tests.py
@@ -1,3 +1,14 @@
+import unittest
+from django.urls import reverse
 from django.test import TestCase
+from .models import Products
 
-# Create your tests here.
+class ProductListTest(TestCase):
+    def setUp(self):
+        self.url_all_products = reverse('get_all_products')
+        self.url_one_product = reverse('get_one_product')  # test for get on product by code
+
+    def test_status_code(self):
+        # response = self.client.get(self.url_all_products)
+        response = self.client.get(self.url_one_product)
+        self.assertEqual(response.status_code, 200)
