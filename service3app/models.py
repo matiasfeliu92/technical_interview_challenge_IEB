bb941c8d4445cbfc8c812faadf19616d32a5b083
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-24 11:54:13 -0300

    refactor: add unique=True atributte to code field in database

--- service3app/models.py
+++ service3app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    code = models.CharField(max_length=20)
+    code = models.CharField(max_length=20, unique=True)
     purchase_price = models.IntegerField()
     sale_price = models.IntegerField()
     description = models.TextField()
