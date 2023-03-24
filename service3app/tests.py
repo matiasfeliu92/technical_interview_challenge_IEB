8da228a6793da799549e8326029245950943d735
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-24 11:55:11 -0300

    refactor: change tests functions by using requests and pytest modules

--- service3app/tests.py
+++ service3app/tests.py
@@ -1,14 +1,33 @@
 import unittest
 from django.urls import reverse
 from django.test import TestCase
-from .models import Products
+import requests
 
-class ProductListTest(TestCase):
-    def setUp(self):
-        self.url_all_products = reverse('get_all_products')
-        self.url_one_product = reverse('get_one_product')  # test for get on product by code
+def test_get_all_products():
+    response = requests.get('http://localhost:8000/products')
+    assert response.status_code == 200
 
-    def test_status_code(self):
-        # response = self.client.get(self.url_all_products)
-        response = self.client.get(self.url_one_product)
-        self.assertEqual(response.status_code, 200)
+def test_get_one_product():
+    response = requests.get('http://localhost:8000/products/a04')
+    res_body = response.json()
+    assert res_body["code"] == "a04"
+
+def test_create_product():
+    body = {
+        "code": "a06",
+        "purchase_price": 754757,
+        "sale_price": 4634643,
+        "description": "fhfdhgffjf"
+    }
+    response = requests.post('http://localhost:8000/create', body)
+    assert response.status_code == 201
+
+def test_update_product():
+    body = {
+        "code": "a06",
+        "purchase_price": 754757,
+        "sale_price": 4634643,
+        "description": "MMMMMMMMMMMM"
+    }
+    response = requests.post('http://localhost:8000/update/a06', body)
+    assert response.status_code == 201
