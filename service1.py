6c5d31a376a82559e95ade7238aa47645f639014
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-28 14:51:57 -0300

    refactor: update .gitignore file and update service1.py to practice creating a conection between client and server by socket

--- service1.py
+++ service1.py
@@ -1,16 +1,36 @@
 import socket
-from flask import Flask, jsonify
-import requests
+import json
 
-app = Flask(__name__)
+# Establecer la dirección IP y el puerto del servidor localhost
+server_address = ('127.0.0.1', 8000)
 
-@app.route("/product/<string:code>")
-def product(code):
-    response = requests.get(f"http://localhost:8000/products/{code}")
-    if response.status_code == 200:
-        return jsonify(response.json(), 200)
-    elif response.status_code == 404:
-        return jsonify(response.json(), 404)
+# Crear un objeto de socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+# Conectar el socket al servidor localhost
+sock.connect(server_address)
+
+# Enviar la solicitud HTTP GET al servidor
+request = "GET /products/a24 HTTP/1.1\r\nHost: 127.0.0.1:8000\r\n\r\n"
+sock.sendall(request.encode())
+
+# Recibir la respuesta del servidor
+response = ""
+while True:
+    data = sock.recv(1024)
+    if not data:
+        break
+    print(data)
+    response += data.decode()
+
+
+    # Extraer los datos JSON de la respuesta del servidor
+    print(response)
+    # json_data = response.split('\r\n\r\n')
+    # parsed_data = json.loads(json_data)
+
+    # Imprimir los datos JSON
+    print(response)
+
+    # Cerrar la conexión del socket
+    sock.close()
